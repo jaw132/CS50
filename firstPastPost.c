71b982f1a608e4c8376ce9bcc9f87a677b6e9cb6
jaw132 <62038034+jaw132@users.noreply.github.com>
2020-06-13 10:57:19 +0100

    Determine winner of first past the post election

---
+++ firstPastPost.c
@@ -0,0 +1,107 @@
+/* The goal of the exercise is to complete a given template for registering votes in a mock election
+and return the winner. CS50 provided most of the code, I have commented which parts are template and which are 
+my work */
+
+/* CS50 template starts here */
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+/* CS50 TEMPLATE CODE ENDS HERE */
+
+
+/* FROM HERE IS MY CONTRIBUTION */
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    for (int i=0; i<candidate_count; i++){
+        if (strcmp(name, candidates[i].name) == 0){
+            candidates[i].votes += 1;
+            return true;
+        }
+    }
+
+
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int maxvotes = 0;
+
+    for (int i=0; i<candidate_count; i++){
+        if (candidates[i].votes > maxvotes){
+            maxvotes = candidates[i].votes;
+        }
+    }
+
+    for (int i=0; i<candidate_count; i++){
+        if (candidates[i].votes == maxvotes){
+            printf("%s\n", candidates[i].name);
+        }
+    }
+
+    return;
+}
